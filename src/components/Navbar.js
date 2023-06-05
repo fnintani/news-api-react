14a6096d940791d5084b9ea8ef9475ed864d4069
Fitria Intani <nanditama.intani99@gmail.com>
2023-06-05 13:29:45 +0700

    change navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-import "./style.css"
+import "./style.css";
 
 export default function Navbar(props) {
-	return (
-		 <nav className="navbar">
-            <a className="link-home" onClick={props.handleClick} href="#">Home</a>
-         </nav>
-	)
+  return (
+    <nav className="navbar">
+      <h4 className="app-name">NEWS APP</h4>
+      <a className="link-home" onClick={props.handleClick} href="#">
+        Home
+      </a>
+    </nav>
+  );
 }
